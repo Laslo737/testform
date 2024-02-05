a56bf3cfe0cb2f0cc52e6954d4eb18ade81e4ef4
Nikita <powar810321@gmail.com>
2024-02-05 15:35:22 +0400

    test2

--- functions/sendmessage.js
+++ functions/sendmessage.js
@@ -1,4 +1,58 @@
-export async function onRequest(context) {
-  const { request } = context
-  return new Response('Hello Cuka')
+export async function onRequestPost(context) {
+  try {
+    console.log(context);
+    const telegramBotToken = '5458783596:AAGrAKN5Su7ApVD6LeWKfQ6bosVk1V8bxZ4';
+    const telegramChatId = '-860374952';
+
+    // Разбор данных из формы
+    // const formData = JSON.parse(event.body);
+    // const { Недвижимость, Спальня, Бюджет, Имя, Ватсап, Телефон } = formData;
+
+    // // Получение гет-параметров
+    // const urlParams = new URLSearchParams(event.queryStringParameters);
+    // const sub1 = urlParams.get('sub1') || '';
+    // const keyword = urlParams.get('keyword') || '';
+    // const gclid = urlParams.get('gclid') || '';
+
+    // // Формирование текста сообщения для Telegram с добавлением гет-параметров
+    // const message = `
+    //   Новая заявка!
+    //   Недвижимость: ${Недвижимость}
+    //   Спальня: ${Спальня}
+    //   Бюджет: ${Бюджет}
+    //   Имя: ${Имя}
+    //   Ватсап: ${Ватсап || 'Не указан'}
+    //   Телефон: ${Телефон || 'Не указан'}
+    //   sub1: ${sub1}
+    //   keyword: ${keyword}
+    //   gclid: ${gclid}
+    // `;
+
+    // Отправка сообщения в Telegram
+    const telegramApiUrl = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
+    await fetch(telegramApiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        chat_id: telegramChatId,
+        text: 'message',
+      }),
+    });
+
+    // Возвращаем успешный ответ
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Форма успешно отправлена в Telegram!' }),
+    };
+  } catch (error) {
+    console.error('Ошибка обработки формы:', error);
+
+    // Возвращаем ответ об ошибке
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Произошла ошибка при обработке формы.' }),
+    };
+  }
 }
