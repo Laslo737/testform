e45443670e0ae7f2b16772080c99b80f82def8c8
Nikita <powar810321@gmail.com>
2024-02-05 15:55:29 +0400

    resp

--- functions/sendmessage.js
+++ functions/sendmessage.js
@@ -6,6 +6,19 @@ export async function onRequestPost(context) {
     const telegramChatId = '-860374952';
 
     let input = await context.request.formData();
+
+    let output = {};
+    for (let [key, value] of input) {
+      let tmp = output[key];
+      if (tmp === undefined) {
+        output[key] = value;
+      } else {
+        output[key] = [].concat(tmp, value);
+      }
+    }
+
+    let pretty = JSON.stringify(output, null, 2);
+
     // Разбор данных из формы
     // const formData = JSON.parse(event.body);
     // const { Недвижимость, Спальня, Бюджет, Имя, Ватсап, Телефон } = formData;
@@ -44,7 +57,11 @@ export async function onRequestPost(context) {
     });
 
     // Возвращаем успешный ответ
-    return new Response(input, { status: 200, text: "Все супер" });
+    return new Response(pretty, {
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+    });
   } catch (error) {
     console.error('Ошибка обработки формы:', error);
 
