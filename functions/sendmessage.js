bdfc589b5535724888477a31a89a8882672683a1
Nikita <powar810321@gmail.com>
2024-02-09 12:54:43 +0400

    checkRedir

--- functions/sendmessage.js
+++ functions/sendmessage.js
@@ -32,7 +32,7 @@ export async function onRequestPost(context) {
     // Возвращаем успешный ответ
     return new Response(pretty, {
       headers: {
-        'Content-Type': 'application/json;charset=utf-8',
+        'Location': './thanks.html?name="name',
       },
     });
   } catch (error) {
